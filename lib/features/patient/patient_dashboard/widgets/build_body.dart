e6ee24e7676f432d7d7c21b058fcebb575e5b08e
sslp-do <sslp2girl@gmail.com>
2026-05-07 19:05:51 +0300

    Remove unused test page
    - Delete `lib/features/patient/test_page.dart` containing unused Firestore test code.
    - Remove references to `TestPage` in `build_body.dart`.

--- lib/features/patient/patient_dashboard/widgets/build_body.dart
+++ lib/features/patient/patient_dashboard/widgets/build_body.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:smart_glove/features/patient/my_reports/screen/my_reports.dart';
 import 'package:smart_glove/features/patient/patient_overview/screen/patient_overview.dart';
 import 'package:smart_glove/features/patient/settings/screen/settings.dart';
-import 'package:smart_glove/features/patient/test_page.dart';
 
 Widget buildBody(String routeName) {
   switch (routeName) {
